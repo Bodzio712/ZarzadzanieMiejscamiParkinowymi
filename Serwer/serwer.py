79d393ff78d0b40679d6ddde0f27bf6f27362274
Piotr Pogodzinski <ppogodzinski@vp.pl>
2019-11-24 15:44:29 +0000

    Działa SELECT z bazy

--- Serwer/serwer.py
+++ Serwer/serwer.py
@@ -1,7 +1,19 @@
 from flask import Flask, jsonify
 
+import psycopg2
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def main():
-    return jsonify(number='6')
+    connection = psycopg2.connect(user="piotr",
+            password="windows7",
+            host="127.0.0.1",
+            port="5432",
+            database="pgs")
+
+    cursor = connection.cursor()
+    query = "SELECT number, date FROM data ORDER BY date DESC LIMIT 1"
+    cursor.execute(query)
+    anwser = cursor.fetchone()
+    return jsonify(number=anwser[0], date=anwser[1])
