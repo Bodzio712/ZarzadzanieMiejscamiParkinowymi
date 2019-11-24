2c805ae07abfb95a159ecbfd0b020e9040b200cd
Piotr Pogodzinski <ppogodzinski@vp.pl>
2019-11-24 20:16:29 +0000

    Dodana strona home

--- Serwer/serwer.py
+++ Serwer/serwer.py
@@ -1,11 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
 import psycopg2
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
-def main():
+def select_one():
     connection = psycopg2.connect(user="piotr",
             password="windows7",
             host="127.0.0.1",
@@ -16,4 +15,14 @@ def main():
     query = "SELECT number, date FROM data ORDER BY date DESC LIMIT 1"
     cursor.execute(query)
     anwser = cursor.fetchone()
+    return anwser
+
+@app.route('/api', methods=['GET'])
+def api():
+    anwser=select_one()
     return jsonify(number=anwser[0], date=anwser[1])
+
+@app.route('/', methods=['GET'])
+def home():
+    anwser=select_one()
+    return render_template('home.html', number=anwser[0], date=anwser[1])
